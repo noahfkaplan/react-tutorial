5ae12f233088b8e034d55eea6020a430d215530c
Noah Kaplan <nkaplan1@uncc.edu>
2018-10-29 19:10:25 -0400

    initial game finished

--- src/index.js
+++ src/index.js
@@ -10,74 +10,97 @@ function Square(props){
     );
 }
   
-  class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = 
-        {
-            squares: Array(9).fill(null),
-            turnX : true
-        };
+class Board extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = 
+      {
+          squares: Array(9).fill(null),
+          turnX : true
+      };
+  }
+  handleClick(i){
+      if(this.state.squares[i] || CalculateWinner(this.state.squares)){
+          return;
+      }
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.turnX ? 'X':'O';
+      this.setState({squares: squares, turnX: !this.state.turnX});
+  }
+  renderSquare(i) {
+    return(
+      <Square 
+          value = {this.state.squares[i]} 
+          onClick = { () => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = CalculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'The winner is: ' + (winner);
     }
-    handleClick(i){
-        if(this.state.squares[i]){
-            return;
-        }
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.turnX ? 'X':'O';
-        this.setState({squares: squares, turnX: !this.state.turnX});
+    else{
+      status = 'Next player: ' + (this.state.turnX ? 'X':'O');
     }
-    renderSquare(i) {
-      return(
-        
-        <Square 
-            value = {this.state.squares[i]} 
-            onClick = { () => this.handleClick(i)}
-        />
-      );
-    }
-  
-    render() {
-      const status = 'Next player: ' + (this.state.turnX ? 'X':'O');
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+function CalculateWinner(squares){
+  const lines = 
+  [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
     }
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+}
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
         </div>
-      );
-    }
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
+}
   
   // ========================================
   
